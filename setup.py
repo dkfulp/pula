4e1dacf80bb69d5973685197bb490ec238944567
Dakota Fulp <dkfulp@coastal.edu>
2018-04-24 12:46:22 -0400

    Added Pula to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pula',
-      version='1.0.0',
+      version='1.0.0.dev1',
       description='Convienent add-on library for Python, providing frequently used functions.',
       url='https://github.com/dkfulp/pula',
       author='dkfulp',
       author_email='dkfulp@coastal.edu',
-      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      license = 'MIT',
+      keywords='convenient scripting tools',
       packages=['pula'],
-      zip_safe=False)
+      #install_requires=[]
+      )
